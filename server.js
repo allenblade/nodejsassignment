c9e7e2b504f09545cd066bb5211db8cfc12620e3
Alan <Alan@LAPTOP-O0J60BJU>
2021-12-12 20:05:55 +0700

    add end

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ var port = process.env.PORT || 1335;
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.write('Hello Alibaba Cloud\n');
-    //res.end('Hello World\n' + res.connection.localPort);
+    res.end('Hello World\n' + res.connection.localPort);
 }).listen(port);
