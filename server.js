611d5bd78905fe78e835e7f8c9e4843b6dc711f2
Alan <Alan@LAPTOP-O0J60BJU>
2021-12-13 21:15:55 +0700

    add picture

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 'use strict';
 var http = require('http');
 var port = process.env.PORT || 1335;
+const imgURL = "https://aliassignment.oss-ap-southeast-3.aliyuncs.com/download.png";
 
 http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.write('Hello Alibaba Cloud\n');
-    res.end('Hello World\n' + res.connection.localPort);
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write('<h1>Hello Alibaba Cloud</h1><br>');
+    res.end("<img src = \"" + imgURL + "\"/>");
+    //res.end('Hello World\n' + res.connection.localPort);
 }).listen(port);
